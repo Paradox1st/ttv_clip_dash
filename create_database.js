7fa462605526867ed26d105b8063f14df223ec19
Paradox1st <6237816+Paradox1st@users.noreply.github.com>
2019-06-09 17:08:04 -0400

    fixed create_database.js to create/reset database

--- create_database.js
+++ create_database.js
@@ -1,26 +1,18 @@
 'use strict'
 
 var dbinfo = require("./credentials/dbinfo.json"),
-  mysql = require('mysql');
+    mysql = require('mysql');
 
-var db = mysql.createConnection(dbinfo)
+var db = mysql.createConnection(dbinfo);
+db.query('DROP database IF EXISTS ttv_clip_dash;');
+db.query('create database ttv_clip_dash;');
+db.query('use ttv_clip_dash;');
+db.query(`create table users (id int key,
+    username varchar(20) not null,
+    dispname varchar(50) character set utf8mb4 not null,
+    img varchar(255) not null);`);
+db.query(`create table clips (clip_id int key,
+    user_id int,
+    foreign key (user_id) references users(id));`);
 
-db.connect((err)=>{
-  if(err){console.log(err)}
-  else{
-    db.query('DROP database IF EXISTS ttv_clip_dash;')
-    db.query('create database ttv_clip_dash;');
-    db.query('use ttv_clip_dash;');
-    db.query(`create table users (id int key,
-        username varchar(20) not null,
-        dispname varchar(50) character set utf8mb4 not null,
-        img varchar(255) not null);`);
-    db.query(`create table clips (clip_id int key,
-        user_id int,
-        foreign key (user_id) references users(id));`);
-    
-    db.end();
-    
-    console.log('Database Created');
-  }
-});
+db.end();
